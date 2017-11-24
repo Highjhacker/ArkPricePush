02f403c673c0297438da006d967b99a312932d77
highjhacker <mc-somango@live.fr>
2017-11-24 01:32:31 +0100

    Now can convert to any currency accepted by CoinMarketCap

--- index.js
+++ index.js
@@ -3,22 +3,62 @@ const notifier = require('node-notifier');
 const path = require('path');
 var emoji = require('node-emoji');
 
-function get_price(callback) {
-    request({url: 'https://api.coinmarketcap.com/v1/ticker/ark/', json:true}, function(error, response, body) {
+var currencies = {
+    'aud': '$',
+    'brl': '',
+    'cad': '$',
+    'aud': '',
+    'chf': '',
+    'clp': '',
+    'cny': '',
+    'czk': '',
+    'dkk': '',
+    'eur': '€',
+    'gbp': '',
+    'hkd': '',
+    'huf': '',
+    'idr': '',
+    'ils': '',
+    'inr': '',
+    'jpy': '',
+    'krw': '',
+    'mxn': '',
+    'myr': '',
+    'nok': '',
+    'nzd': '',
+    'php': '',
+    'pkr': '',
+    'pln': '',
+    'rub': '',
+    'sek': '',
+    'sgd': '',
+    'thb': '',
+    'try': '',
+    'twd': '',
+    'zar': '',
+    'usd': '$'
+};
 
-        if (error) {
-            return callback(error);
-        }
-        try {
-            if (parseFloat(body[0].percent_change_1h) > 0.00) {
-                callback(null, body[0].price_usd + "$ " + emoji.emojify(":small_red_triangle:"));
+function get_price(callback, currency) {
+    request({
+        url: "https://api.coinmarketcap.com/v1/ticker/ark/?convert=" + (currency ? currency : ''),
+        json:true
+        },
+        function(error, response, body) {
+            if (error) {
+                return callback(error);
             }
-            else {
-                callback(null, body[0].price_usd + "$ " + emoji.emojify(":small_red_triangle_down:"));
+            try {
+
+                if (parseFloat(body[0].percent_change_1h) > 0.00) {
+                    callback(null, parseFloat(body[0]["price_" + currency]).toFixed(2) + currencies[currency] + emoji.emojify(":small_red_triangle:"));
+                }
+                else {
+                    callback(null, parseFloat(body[0]["price_" + currency]).toFixed(2) + currencies[currency] + emoji.emojify(":small_red_triangle_down:"));
+                }
+            } catch (ex) {
+                callback(ex);
             }
-        } catch (ex) {
-            callback(ex);
-        }
     });
 }
 
@@ -30,5 +70,5 @@ setInterval(function() {
             icon: path.join(__dirname, 'logo.png'),
             timeout: 15
         });
-    });
+    }, "usd");
 }, 60000);
